604921ea463e5968dcb744c4a2844502688c0d02
Supunchamara98 <125036389+Supunchamara98@users.noreply.github.com>
2023-03-01 22:14:28 +0530

    Add files via upload
    Finished The Back end

---
+++ ContextFile.cs
@@ -0,0 +1,21 @@
+﻿using Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace Library
+{
+    public class ContextFile : DbContext
+    {
+        public ContextFile(DbContextOptions options) : base(options)
+        {
+        }
+
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Owner> Owners { get; set; }
+
+        public DbSet<Ussers> Ussers { get; set; }
+
+    }
+}
